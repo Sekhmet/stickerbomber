fed1cabada46d29ada17d1c95d26b3a3158a7d5f
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2020-07-04 17:15:46 +0200

    feat: add split pane

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders settings pane", () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/Settings/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("renders result pane", () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/Result/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import SplitPane from "react-split-pane";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitPane split="vertical" minSize={50} defaultSize={300}>
+      <div>Settings</div>
+      <div>Result</div>
+    </SplitPane>
   );
 }
 
